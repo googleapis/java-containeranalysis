6f03c8d80f65cc4a949b032fb2c9967b7606a4a3
Emily Ball <emilyball@google.com>
2021-08-11 15:39:28 -0700

    chore: format

--- google-cloud-containeranalysis/src/test/java/com/google/cloud/devtools/containeranalysis/v1/ITGrafeasInteropTest.java
+++ google-cloud-containeranalysis/src/test/java/com/google/cloud/devtools/containeranalysis/v1/ITGrafeasInteropTest.java
@@ -39,11 +39,9 @@ public class ITGrafeasInteropTest {
     GrafeasSettings grafeasSettings = grafeasClient.getSettings();
 
     assertEquals(settings.getClock(), grafeasSettings.getClock());
-    assertEquals(
-        settings.getBackgroundExecutorProvider(), grafeasSettings.getExecutorProvider());
+    assertEquals(settings.getBackgroundExecutorProvider(), grafeasSettings.getExecutorProvider());
     assertEquals(settings.getEndpoint(), grafeasSettings.getEndpoint());
-    assertEquals(
-        settings.getBackgroundExecutorProvider(), grafeasSettings.getExecutorProvider());
+    assertEquals(settings.getBackgroundExecutorProvider(), grafeasSettings.getExecutorProvider());
     assertEquals(settings.getHeaderProvider(), grafeasSettings.getHeaderProvider());
     assertEquals(
         settings.getTransportChannelProvider(), grafeasSettings.getTransportChannelProvider());
